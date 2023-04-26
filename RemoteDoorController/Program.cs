3267ae1fb922693ef75fe8582d709bd5313b7c53
Cody Goodman <cody@letcodyhandleit.com>
2023-04-26 12:42:28 -0400

    Working Remote Door Scripts

---
+++ RemoteDoorController/Program.cs
@@ -0,0 +1,32 @@
+﻿using Sandbox.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        MyCommandLine _commandLine = new MyCommandLine();
+        string BROADCAST_TAG = "rcdoors";
+        public void Main(string argument, UpdateType updateSource)
+        {
+            if (_commandLine.TryParse(argument)) {
+                if (_commandLine.ArgumentCount != 1)
+                {
+                    Echo("Invalid number of arguments. Expected 1 but received: " + _commandLine.ArgumentCount);
+                }
+                string doorName = _commandLine.Argument(0);
+                string message = _commandLine.Switch("single") ? "single" : "group";
+                if (_commandLine.Switch("open"))
+                    message = message + ";" + doorName + ";open";
+                else if (_commandLine.Switch("close"))
+                    message = message + ";" + doorName + ";close";
+                else
+                {
+                    message = message + ";" + doorName + ";toggle";
+                }
+                Echo("Sending Message: " + message);
+                IGC.SendBroadcastMessage(BROADCAST_TAG, message);
+            }
+        }
+    }
+}
--- RemoteDoorListener/Program.cs
+++ RemoteDoorListener/Program.cs
@@ -1,28 +1,13 @@
-﻿using Sandbox.Game.EntityComponents;
-using Sandbox.ModAPI.Ingame;
-using Sandbox.ModAPI.Interfaces;
-using SpaceEngineers.Game.ModAPI.Ingame;
-using System;
-using System.Collections;
+﻿using Sandbox.ModAPI.Ingame;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Text;
-using VRage;
-using VRage.Collections;
-using VRage.Game;
-using VRage.Game.Components;
-using VRage.Game.GUI.TextPanel;
-using VRage.Game.ModAPI.Ingame;
-using VRage.Game.ModAPI.Ingame.Utilities;
-using VRage.Game.ObjectBuilders.Definitions;
-using VRageMath;
 
 namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
         string TAG = "rcdoors";
+        string lastError = "";
         public Program()
         {
             Runtime.UpdateFrequency = UpdateFrequency.Update10;
@@ -30,11 +15,67 @@ namespace IngameScript
 
         public void Main(string argument, UpdateType updateSource)
         {
+            Echo("Last Error: \n\n" + lastError);
             IMyBroadcastListener listener = IGC.RegisterBroadcastListener(TAG);
-            while(listener.HasPendingMessage)
+            while (listener.HasPendingMessage)
             {
-
+                MyIGCMessage message = listener.AcceptMessage();
+                Echo("Receiving Message: " + message.Data.ToString());
+                if (message.Data is string)
+                {
+                    string data = message.Data.ToString();
+                    string [] splits = message.Data.ToString().Split(';');
+                    if (splits.Count() != 3)
+                        lastError ="Received malform message: " + data + "\n\nFrom: " + GridTerminalSystem.GetBlockWithId(message.Source).CustomName;
+                    else
+                        Handle(splits[0], splits[1], splits[2]);
+                }
+                else
+                {
+                    lastError = "Received Message With Invalid Data Type From: " + message.Source;
+                }
             }
         }
+
+        private void Handle(string type, string name, string action)
+        {
+            List<IMyDoor> doors = new List<IMyDoor>();
+            if (type.Equals("group"))
+            {
+                IMyBlockGroup group = GridTerminalSystem.GetBlockGroupWithName(name);
+                group.GetBlocksOfType(doors);
+            }
+            else if (type.Equals("single"))
+            {
+                IMyDoor door = (IMyDoor)GridTerminalSystem.GetBlockWithName(name);
+                doors.Add(door);
+            }
+            else
+            {
+                lastError = "Received Invalid Type: " + type + "\n\nMust be either \'single\' or \'group\'";
+            }
+            Handle(doors, action);
+        }
+
+        private void Handle(List<IMyDoor> doors, string action)
+        {
+            if (action.Equals("open"))
+            {
+                doors.ForEach(door => door.OpenDoor());
+            }
+            else if (action.Equals("close"))
+            {
+                doors.ForEach(door => door.CloseDoor());
+            }
+            else if (action.Equals("toggle"))
+            {
+                doors.ForEach(door => door.ToggleDoor());
+            }
+            else
+            {
+               lastError = "Unrecognized door action: " + action + "\n\nMust be \'open' or \'close\' or \'toggle\'";
+            }
+        }
+
     }
 }
